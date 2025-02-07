024672cefae209110423cb81f54664a63a33dc9f
Yash Sahay <yashsahay999@gmail.com>
2025-02-07 23:54:50 +0530

    Created Servlet by extending GenericServlet class. Also defined the servlet path inside web.xml.

---
+++ src/main/java/servlets/SecondServlet.java
@@ -0,0 +1,16 @@
+package servlets;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+
+public class SecondServlet extends GenericServlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("SecondServlet is invoked!");
+    }
+}
